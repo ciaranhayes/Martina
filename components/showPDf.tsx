c0459d305cfe04fdc394dcb69574d27751416401
Ciaran Hayes <hayes.c.hayes@gmail.com>
2025-09-07 16:02:38 +0200

    Pdf fixing

--- components/showPDf.tsx
+++ components/showPDf.tsx
@@ -1,13 +1,20 @@
 export default function PDFViewer() {
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+    if (isMobile) {
+        return (
+            <div style={{ textAlign: "center", marginTop: "50px" }}>
+                <p>Viewing PDFs is limited on mobile. Please download:</p>
+                <a href="/diss.pdf" download>Download PDF</a>
+            </div>
+        );
+    }
+
     return (
         <embed
             src="/diss.pdf"
             type="application/pdf"
-            style={{
-                width: "100vw",
-                height: "100vh",
-                border: "none"
-            }}
+            style={{ width: "100vw", height: "100vh", border: "none" }}
         />
     );
 }
